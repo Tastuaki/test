9bad0576fd40b50f7d8dd378f9d2cd98b976ed31
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-04-13 10:50:27 +0900

    c

--- music.js
+++ music.js
@@ -14,6 +14,8 @@ const result = fetch("https://github.com/Tastuaki/OPED", {
 // console.log(music_list)
 // console.log(music_list.text())
 const music = new Audio('https://github.com/Tastuaki/OPED/blob/main/C%C2%B3%20-%E3%82%B7%E3%83%BC%E3%82%AD%E3%83%A5%E3%83%BC%E3%83%96-%20OP1(Endless%20Story).mp3?raw=true');
+
+const title = document.getElementById('title');
 const play = document.getElementById('play');
 const mute = document.getElementById('mute');
 const loop = document.getElementById('loop');
@@ -22,11 +24,12 @@ const volume_text = document.getElementById('volume-text');
 // 再生ボタン
 play.addEventListener('click', function(){
   if(!music.paused){
-    play.innerHTML ='<i class="fa-solid fa-play"></i>';
+    play.innerHTML ='<i class="fas fa-play"></i>';
     music.pause();
   }else{
-    play.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    play.innerHTML = '<i class="fas fa-pause"></i>';
     music.play();
+    title.innerHTML = '<i class="fas fa-music"></i>'+" "
   }
 });
 
@@ -45,10 +48,11 @@ var intervalId = setInterval(check_sound, 1000);
 function check_sound(){
   console.log(music.ended)
   if(music.ended){
-    play.innerHTML ='<i class="fa-solid fa-play"></i>';
+    play.innerHTML ='<i class="fas fa-play"></i>';
     music.src = 'https://raw.githubusercontent.com/Tastuaki/OPED/main/CHAOS%3BHEAD%20ED.mp3';
     music.play();
-    play.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    play.innerHTML = '<i class="fas fa-pause"></i>';
+    title.innerHTML = '<i class="fas fa-music"></i>'+" "
   }
 }
 
@@ -56,10 +60,10 @@ function check_sound(){
 mute.addEventListener('click', function(){
   if(music.muted){
     music.muted = false;
-    mute.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+    mute.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }else{
     music.muted = true;
-    mute.innerHTML = '<i class="fa-solid fa-volume"></i>';
+    mute.innerHTML = '<i class="fas fa-volume"></i>';
   }
 });
 
@@ -67,9 +71,9 @@ mute.addEventListener('click', function(){
 loop.addEventListener('click', function(){
   if(music.loop){
     music.loop = false;
-    loop.innerHTML = '<i class="fa-solid fa-repeat"></i>';
+    loop.innerHTML = '<i class="fas fa-repeat"></i>';
   }else{
     music.loop = true;
-    loop.innerHTML = '<i class="fa-solid fa-rotate"></i>';
+    loop.innerHTML = '<i class="fas fa-sync"></i>';
   }
 });
