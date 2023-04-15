e83fb17446b9d534d54441e4b81c1d612e7966c1
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-04-15 19:40:05 +0900

    c

--- music.js
+++ music.js
@@ -7,8 +7,8 @@ reader.onload = function () {
 	console.log(reader.result);
 }
 reader.readAsText(file);
-fname = reader.result
-console.log(fname[0])
+fname = reader.result;
+console.log(fname[0]);
 
 const music = new Audio('https://github.com/Tastuaki/OPED/blob/main/C%C2%B3%20-%E3%82%B7%E3%83%BC%E3%82%AD%E3%83%A5%E3%83%BC%E3%83%96-%20OP1(Endless%20Story).mp3?raw=true');
 
