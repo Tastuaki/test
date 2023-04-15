4730b8093795034a10df77a868ee19a1e593082c
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-04-15 21:16:40 +0900

    c

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var fname = [""];
-var file_property = {type: "text/plain"}
+var cnt = 0;
 const xhr = new XMLHttpRequest();
 xhr.open('get', 'https://raw.githubusercontent.com/Tastuaki/OPED/main/%2Btitle.txt');
 xhr.send();
@@ -13,7 +13,7 @@ xhr.onreadystatechange = function() {
 }
 console.log(fname[0]);
 
-const music = new Audio('https://github.com/Tastuaki/OPED/blob/main/%E4%B8%AD%E4%BA%8C%E7%97%85%E3%81%A7%E3%82%82%E6%81%8B%E3%81%8C%E3%81%97%E3%81%9F%E3%81%84%EF%BC%81%20-Take%20On%20Me-%20%E6%8C%BF%E5%85%A5%E6%AD%8C.mp3?raw=true');
+const music = new Audio('https://github.com/Tastuaki/OPED/blob/main/'+fname[cnt]+'?raw=true');
 
 const title = document.getElementById('title');
 const play = document.getElementById('play');
@@ -29,7 +29,7 @@ play.addEventListener('click', function(){
   }else{
     play.innerHTML = '<i class="fas fa-pause"></i>';
     music.play();
-    title.innerHTML = '<i class="fas fa-music"></i>'+" "
+    title.innerHTML = '<i class="fas fa-music"></i>'+ fname[cnt]
   }
 });
 
@@ -41,10 +41,11 @@ function check_sound(){
   console.log(music.ended)
   if(music.ended){
     play.innerHTML ='<i class="fas fa-play"></i>';
-    music.src = 'https://raw.githubusercontent.com/Tastuaki/OPED/main/CHAOS%3BHEAD%20ED.mp3';
+    cnt = cnt + 1;
+    music.src = 'https://github.com/Tastuaki/OPED/blob/main/'+fname[cnt]+'?raw=true';
     music.play();
     play.innerHTML = '<i class="fas fa-pause"></i>';
-    title.innerHTML = '<i class="fas fa-music"></i>'+" 次の曲"
+    title.innerHTML = '<i class="fas fa-music"></i>'+ fname[cnt]
   }
 }
 
