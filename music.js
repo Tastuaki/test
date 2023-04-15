56762e67c1f42aaf5f9f019092619d6aaf9f841e
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-04-15 23:49:37 +0900

    c

--- music.js
+++ music.js
@@ -17,7 +17,7 @@ xhr.onreadystatechange = function() {
     while(true){
       fname[n] += data[i];
       if(fname[n].includes("\n")){
-        fname[n].slice(0,-1)
+        fname[n] = fname[n].slice(0,-1)
         console.log(fname[n]);
         n += 1;
       }
