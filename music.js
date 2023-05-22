e6c4e263140ec8bef91af1eef4a29f658bb8ff58
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-05-22 11:46:27 +0900

    c

--- music.js
+++ music.js
@@ -24,6 +24,9 @@ xhr.onreadystatechange = function() {
       if(fname[n].includes("\n")){
         fname[n] = fname[n].replace("undefined","");
         fname[n] = fname[n].slice(0,-1);
+        if(fname[n].includes("\n")){
+          console.log(fname[n]);
+        }
         for(j=0; j < btext.length; j++){
           titletext[n] = fname[n].replace(btext[j],ctext[j]);
         }
