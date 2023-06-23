74f86e4c81e94a13d62c40ca26a1d63a3bb047d4
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-06-23 17:27:03 +0900

    c

--- music.js
+++ music.js
@@ -65,10 +65,9 @@ xhr.onreadystatechange = function() {
 }
 
 function make_list(sig){
-  let i=0;
   if(sig != -1){
     mlist.innerHTML = '';
-    for(;i < titletext.length;i++){
+    for(i = 0;i < titletext.length;i++){
       var li = document.createElement('li');
       li.innerHTML = '<button id="smusic" value="'+ i + '" onclick="list_select('+ i +')"><label class="tt">' + musictitle[i] + '</label><label class="tt">' + animetitle[i] + '</label></button>';
       mlist.appendChild(li);
