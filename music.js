78bb57ae51310a5e4b0114697e8cb001c6df4db6
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-08-09 15:08:48 +0900

    list index b2.5

--- music.js
+++ music.js
@@ -356,9 +356,7 @@ function search(key,list){
 // リスト変更
 list.addEventListener('click', function(){
   document.getElementById('full').style.display = "flex"
-  var nowp = window.pageYOffset
-  document.getElementById('full').style.top = nowp
-  // document.getElementById('full').style.top = window.pageYOffset
+  document.getElementById('full').style.top = window.pageYOffset + "px"
   document.getElementById('body').style.overflow = "visible hidden"
   for(let i = 0;i < 2;i++){
     var li = document.createElement('li');
@@ -392,8 +390,8 @@ function autoscroll(){
   var targetbase = document.getElementById("smusic_" + num).getBoundingClientRect().top
   // test.textContent += targetbase + ":" + hheader + " "
   if(targetbase != hheader){
-    test.innerText += targetbase + " : " + targetPosition + " "
     targetPosition = targetbase - (hheader + window.pageYOffset);
+    test.innerText += targetbase + " : " + targetPosition + " "
     window.scrollTo({ top : targetPosition ,behavior: 'smooth'});
   }
 }
