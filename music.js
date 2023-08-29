fa2eca987fbb46394a8646b0122f6e7e02a7ade9
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2023-08-29 11:42:06 +0900

    slide_list

--- music.js
+++ music.js
@@ -156,6 +156,7 @@ const list = document.getElementById('on_list');
 const lists = document.getElementById('lists')
 
 function slide_list(list,data){
+  var i
   for(i = 0;i < list.length-2;i++){
     list[i] = list[i+1]
   }
