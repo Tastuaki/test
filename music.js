d4c7fb7a3b24296b662137ae9f4ee3c244d933f4
Tastuaki <69177468+Tastuaki@users.noreply.github.com>
2024-03-13 16:23:44 +0900

    seekf3

--- music.js
+++ music.js
@@ -307,7 +307,7 @@ function check_sound(){
       cnt = mcnt;
     }
   }else{
-    if(title.innerHTML != ""){
+    if(mds != 0){
       ptt.innerHTML = Math.floor(music.currentTime/60)+":"+('00'+Math.floor(music.currentTime%60)).slice(-2)+" / "+mdm+":"+mds
       ptime.value = music.currentTime / (music.duration / 100)
     }
